4893f695930b2cd7795667705c3930de5904ac1b
Kyle Spannuth <kylespannuth@gmail.com>
2020-12-22 19:03:27 -0500

    changed padding and updated header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -5,23 +5,62 @@ import LightTheme from "../../styles/theme/LightTheme";
 import PropsTheme from "../../styles/theme/PropsTheme";
 import { ThemeContext } from "styled-components"
 import { useContext } from "react";
+const headertexts = [
+    {
+        link: "/",
+        text: "Yesteryear's service with tomorrow's technology."
+    },
+    {
+        link: "/services",
+        text: "Our Services"
+    },
+    {
+        link: "/prepressdesign",
+        text: "Pre-Press & Design"
+    },
+    {
+        link: "/contact",
+        text: "Contact Us"
+    
+    }
+]
 
-export default function Header() {
+export default function Header(props) {
 
     const themeContext = useContext(ThemeContext);
 
+    const getHeaderImagePath = () => {
+        return themeContext === DarkTheme ? "header.jpg" : "header.jpg"
+    }
+
     return (
-        <Wrapper>
-            
-        </Wrapper>
+        <Container>
+            <HeaderSection>
+                <HeaderImage src={`/img/header/${getHeaderImagePath()}`} />
+            </HeaderSection>
+        </Container>
     )
 }
 
-const Wrapper = styled.div`
-    width: 100vw;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: inherit;
+  /* 10 year old chrome bug, use this instead of height 100% */
+  flex-grow: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+const HeaderSection = styled.div`
     display: flex;
-    flex-direction: column;
-    padding: 0 0.5em;
-    /* This will basically push everything to left and right. */
-    justify-content: center;
+    flex-direction: row;
+    width: 100%;
+    padding: 0px;
+`
+
+const HeaderImage = styled.img`
+    width: 100%;
+    height: auto;
 `
