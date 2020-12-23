3d9622b47fb00e6917381c918a1e40dc97ba419e
JasonWet <jason@colonialpress.net>
2020-12-23 00:24:14 -0500

    pre-press & design page created ~ responsive

--- pages/prepressdesign.tsx
+++ pages/prepressdesign.tsx
@@ -2,13 +2,110 @@ import Head from 'next/head'
 import styled from "styled-components";
 export default function PrePressDesign() {
   return (
-    <Wrapper>
-      <h2>Pre-Press & Design</h2>
-    </Wrapper>
+    <Container>
+      <Item>
+        <TextTitle>Design</TextTitle>
+        <Text>We offer a complete line of layout and design options. From single to full color formats. We work closely with our clients to achieve the design they are imagining.</Text>
+      </Item>
+      <Item>
+        <TextTitle>Pre-Press</TextTitle>
+        <Text>We use state of the art technology with a color match proofing system. We work with many different programs and file types for the Mac and PC platforms.</Text>
+      </Item>
+      <Item>
+        <ListsContainer>
+          <Item>
+            <ListTitle>Mac:</ListTitle>
+            <ListContainer>
+              <ListItem>InDesign</ListItem>
+              <ListItem>Illustrator</ListItem>
+              <ListItem>Photoshop</ListItem>
+              <ListItem>QuarkXPress</ListItem>
+              <ListItem>PageMaker</ListItem>
+              <ListItem>Microsoft Word</ListItem>
+              <ListItem>Microsoft Publisher</ListItem>
+            </ListContainer>
+          </Item>
+          <Item>
+            <ListTitle>PC:</ListTitle>
+            <ListContainer>
+              <ListItem>PageMaker</ListItem>
+              <ListItem>QuarkXPress</ListItem>
+              <ListItem>Illustrator</ListItem>
+              <ListItem>Photoshop</ListItem>
+              <ListItem>Microsoft Word</ListItem>
+              <ListItem>Microsoft Publisher</ListItem>
+            </ListContainer>
+          </Item>
+          <Item>
+            <ListTitle>Files Accepted:</ListTitle>
+            <ListContainer>
+              <ListItem>Zip</ListItem>
+              <ListItem>Flash</ListItem>
+              <ListItem>E-Mail</ListItem>
+              <ListItem>FTP Site</ListItem>
+            </ListContainer>
+          </Item>
+        </ListsContainer>
+      </Item>
+    </Container>
   )
 }
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3% 8%;
+  width: 100%;
+  height: inherit;
+  /* 10 year old chrome bug, use this instead of height 100% */
+  flex-grow: 1;
+  justify-content: center;
+  align-items: legt;
 
-const Wrapper = styled.div`
-  padding: 4em;
+  @media(max-width: 1050px) {
+    flex-direction: column;
+  }
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2% 0%;
+`
+
+const TextTitle = styled.h2`
+  font-size: 35px;
+`
+const Text = styled.p`
+
+`
+
+const ListTitle = styled.h2`
+  display: flex;
+  flex-direction: column;
+  font-size: 28px;
+`
+const ListsContainer = styled.div`
+  padding-top: 20px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  flex-wrap: nowrap;
+  height: 100%;
+
+  @media(max-width: 1000px) {
+    flex-wrap: wrap;
+  }
+`
+
+const ListContainer = styled.div`
+  width: 33.33%;
+
+  @media(max-width: 1000px) {
+    width: 100%;
+  }
+`
+
+const ListItem = styled.p`
+
 `
